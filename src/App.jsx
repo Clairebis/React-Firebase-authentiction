4d31dce93a417658da7ffe91a2506dc59a23044e
Claire <eaaclhb@students.eaaa.dk>
2023-10-11 22:55:48 +0200

    Toastify

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ import {
   createUserWithEmailAndPassword,
 } from "firebase/auth";
 import Home from "./Home";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const [email, setEmail] = useState("");
@@ -28,22 +30,31 @@ function App() {
     const authentication = getAuth();
 
     if (id === 2) {
-      createUserWithEmailAndPassword(authentication, email, password).then(
-        (response) => {
-          console.log(response);
+      createUserWithEmailAndPassword(authentication, email, password)
+        .then((response) => {
+          navigate("/home");
           sessionStorage.setItem(
             "Auth Token",
-            response._tokenResponse.refreshToken,
-
-            navigate("/home")
+            response._tokenResponse.refreshToken
           );
-        }
-      );
+        })
+        .catch((error) => {
+          console.log(error);
+          if (error.code === "auth/email-already-in-use") {
+            toast.error("Email Already in Use");
+          }
+          if (error.code === "auth/weak-password") {
+            toast.error("Password should be at least 6 characters");
+          }
+          if (error.code === "auth/invalid-email") {
+            toast.error("Please enter a valid email address");
+          }
+        });
     }
 
     if (id === 1) {
-      signInWithEmailAndPassword(authentication, email, password).then(
-        (response) => {
+      signInWithEmailAndPassword(authentication, email, password)
+        .then((response) => {
           console.log(response);
           sessionStorage.setItem(
             "Auth Token",
@@ -51,14 +62,29 @@ function App() {
 
             navigate("/home")
           );
-        }
-      );
+        })
+        .catch((error) => {
+          console.log(error);
+          if (error.code === "auth/wrong-password") {
+            toast.error("Please check the Password");
+          }
+          if (error.code === "auth/invalid-login-credentials") {
+            toast.error("Please check the Password");
+          }
+          if (error.code === "auth/user-not-found") {
+            toast.error("Please check the Email");
+          }
+          if (error.code === "auth/invalid-email") {
+            toast.error("Please enter a valid email address");
+          }
+        });
     }
   };
 
   return (
     <div className="App">
       <>
+        <ToastContainer />
         <Routes>
           <Route
             path="/login"
@@ -82,6 +108,7 @@ function App() {
               />
             }
           />
+
           <Route path="/home" element={<Home />} />
         </Routes>
       </>
